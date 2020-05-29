69e82baed566cd94a0901487cfa06a69f0241a3a
GraffVishenka <maxvisnh@gmail.com>
2020-05-29 13:20:35 +0300

    Zadanie 1

---
+++ Program (6).cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+using static System.Convert;
+using MyLibrary;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(lib1.stpow("aloha", 5));
+            Console.WriteLine(lib1.rev("arigato"));
+            Console.WriteLine(lib1.remov("plmpaop","p"));
+            Console.WriteLine(lib1.leng("Your personal nightmare."));
+        }
+    }
+}
---
+++ lib1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyLibrary
+{
+    public class lib1
+    {
+        static public string stpow(string a, int b)
+        {
+            string a1 = a;
+            for (int i = 0; i < b - 1; i++)
+            {
+                a = a + a1;
+            }
+            return (a);
+        }
+        static public string rev(string a)
+        {
+            string v = "";
+            for (int i = a.Length - 1; i > -1; i--) 
+            {
+                v = v + a[i] + "";
+            }
+            return (v);
+        }
+        static public string remov(string ds, string s)
+        {
+            for (int i = 0; i < ds.Length; i++)
+            {
+                ds = ds.Replace(s, "");
+            }
+            return (ds);
+        }
+        static public int leng(string ds)
+        {
+            int a = ds.Length;
+            return (a);
+        }
+    }
+}
+
